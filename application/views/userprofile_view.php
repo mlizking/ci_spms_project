03016dc77cde35c58a9905b824babd5bb176770c
mlizking <keeratikinggg@gmail.com>
2019-06-20 22:29:34 +0700

    Update userprofile_view.php

--- application/views/userprofile_view.php
+++ application/views/userprofile_view.php
@@ -19,8 +19,12 @@
                 $status = $row->us_name;
 
     ?>
-                <div class="text-center mt-3">
-                    <h1>User Profile</h1>
+                <div class="container-fluid mt-3">
+                    <div class="pt-3 pb-2 mb-3 border-bottom">
+                        <div class="text-center">
+                        <h1 class="h2">User Profile</h1>
+                        </div>
+                    </div>
                 </div>
                 
                 <div class="container mt-3">
@@ -49,7 +53,29 @@
                     </div>
                     <div class="row text-center mb-3">
                         <div class="col">
-                            <button class="btn btn-warning">Edit</button>
+                            <button class="btn btn-warning" data-toggle="modal" data-target="#exampleModal">Edit</button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            ...
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-primary">Save changes</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>           
